e63775d2c168922dd7af3316de378426fe49f11a
Angelica Grass <ag223vg@student.lnu.se>
2021-01-20 08:56:49 +0100

    Able to write text instead of only numbers

--- src/models/home-model.js
+++ src/models/home-model.js
@@ -2,11 +2,17 @@ import mongoose from 'mongoose'
 
 // Create a schema.
 const schema = new mongoose.Schema({
+  // value: {
+  //   type: String,
+  //   required: '`{PATH}` is required!',
+  //   max: [42, '`{PATH}` ({VALUE}) exceeds the limit ({MAX}).'],
+  //   min: [1, '`{PATH}` ({VALUE}) is beneath the limit ({MIN}).']
+  // }
   value: {
-    type: Number,
-    required: '`{PATH}` is required!',
-    max: [42, '`{PATH}` ({VALUE}) exceeds the limit ({MAX}).'],
-    min: [1, '`{PATH}` ({VALUE}) is beneath the limit ({MIN}).']
+    type: String,
+    // required: true,
+    maxlength: 50,
+    minlength: 2
   }
 }, {
   timestamps: true,
